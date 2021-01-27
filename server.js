fdf4f4bf6d348d97a04b5343545d9bdd155be6ec
Suzy Le Bel <suzy.lebel@gmail.com>
2021-01-26 18:03:27 -0800

    process.env

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 const app = express();
 
@@ -17,7 +18,9 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
 
 // routes
